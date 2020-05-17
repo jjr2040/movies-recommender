6819db3b0a821f072238fd83ca1a409336bd221d
Juan Jose Villegas <juan.villegas@rappi.com>
2020-05-17 10:54:55 -0500

    Added neomodel init

--- moviesapp/moviesapp/settings.py
+++ moviesapp/moviesapp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from neomodel import config
+
+config.DATABASE_URL = 'bolt://neo4j:neo4j@localhost:7687'  # default
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
