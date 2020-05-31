476d953697292cb48d87f3b059519cab33ea67a3
Juan Sebastian Mendez <juansebastian.mendezr@gmail.com>
2020-05-31 16:53:50 -0500

    Added users/index view with pagination and search bar

--- moviesapp/recommender/views.py
+++ moviesapp/recommender/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
+from .models import User
 
-class HomeView(TemplateView):
-    template_name = 'index.html'
+user_list = User.nodes.all()
+
+def index(request):
+    global user_list
+    page = request.GET.get('page', 1)
+
+    local_users = user_list
+    
+    userId = request.GET.get('userId')
+    
+    if userId:
+        local_users = User.nodes.filter(userId=userId)
+
+    paginator = Paginator(local_users, 10)
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
+    return render(request, 'index.html', { 'users': users })
